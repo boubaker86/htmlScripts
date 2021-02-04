dcf363d13b584d6a61fdc9545cb8d3b09e466274
boubaker86 <48968215+boubaker86@users.noreply.github.com>
2021-02-04 10:09:49 +0100

    Create hero_rescue.js

---
+++ hero_rescue.js
@@ -0,0 +1,74 @@
+var gameId = "4000287";
+var videoAdPlacementId = "video";
+var rewardedVideoAdPlacementId = "rewardedVideo";
+var bannerAdPlacementId = "banner";
+var isTest = true;
+var isDebug = true;
+var bannerPosition = "TOP_CENTER";
+
+
+
+function initApp() {
+	// Initialize UnityAds
+	cordova.plugins.UnityAds3.UnityAdsInit(gameId, isTest, isDebug, function callback(error, result){
+            
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(result);
+            }
+
+        });
+	
+	function showVideoAd()	{
+		// Rewarded Video Ads
+		cordova.plugins.UnityAds3.ShowVideoAd(videoAdPlacementId, function callback(error, result){
+			if(error){
+				console.log(error);
+			}
+			else{
+				console.log(result);
+			}
+		});
+	}
+	
+	function showRewardedAd()	{
+		// Video Ads
+		cordova.plugins.UnityAds3.ShowVideoAd(rewardedVideoAdPlacementId, function callback(error, result){
+			if(error){
+				console.log(error);
+			}
+			else{
+				console.log(result);
+			}
+		});
+		
+	}
+	
+	function showBanner(){
+		//Banner Ads
+		cordova.plugins.UnityAds3.ShowBannerAd(bannerAdPlacementId, bannerPosition, function callback(error, result){
+			if(error){
+				console.log(error);
+				setTimeout(function(){showBanner();},20000);
+			}
+			else{
+				console.log(result);
+			}
+		});
+	}
+	showBanner();
+    setInterval(function(){ showVideoAd();}, 90000);
+
+}
+
+
+  
+
+
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
